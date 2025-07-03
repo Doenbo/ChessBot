ef6b77bcb7e47834719391554c9ea96c5a5aed63
Doenbo <dennis.perkuhn@gmail.com>
2025-07-03 15:04:46 +0200

    Moved Files, Created Some Classes and Enums

---
+++ src/pieces/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ChessBot.Enums;
+
+namespace ChessBot.Pieces;
+
+public abstract class Piece
+{
+    protected Piece(Color color)
+    {
+        Color = color;
+    }
+
+    public Color Color { get; }
+
+    public abstract void move();
+}
---
+++ src/pieces/Rook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessBot.Enums;
+
+namespace ChessBot.Pieces;
+
+public class Rook : Piece
+{
+    public Rook(Color color) : base(color)
+    {
+
+    }
+
+    public override void move()
+    {
+        throw new NotImplementedException();
+    }
+}
